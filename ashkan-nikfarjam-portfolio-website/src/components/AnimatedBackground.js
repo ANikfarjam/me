6a93542e72db2aeca39be6e46a249179f9b8d987
ANikfarjam <ashkan.nikfarjam@sjsu.edu>
2026-03-16 20:56:02 -0700

    adding new design assets to repo

---
+++ ashkan-nikfarjam-portfolio-website/src/components/AnimatedBackground.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styling/AnimatedBackground.css';
+
+const AnimatedBackground = () => (
+  <div className="aurora-bg">
+    <div className="aurora-orb aurora-orb-1" />
+    <div className="aurora-orb aurora-orb-2" />
+    <div className="aurora-orb aurora-orb-3" />
+    <div className="aurora-orb aurora-orb-4" />
+    <div className="aurora-grid" />
+  </div>
+);
+
+export default AnimatedBackground;
